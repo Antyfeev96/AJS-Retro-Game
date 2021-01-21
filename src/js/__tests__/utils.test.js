afa384a1988cdfc6b9ded23749feeab4bdedf090
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-01-21 22:14:44 +0300

    Board rendered, tests done

---
+++ src/js/__tests__/utils.test.js
@@ -0,0 +1,46 @@
+import { calcTileType } from '../utils';
+
+test('Upper left corner should be rendered', () => {
+    const upperLeftCorner = calcTileType(0, 64);
+    expect(upperLeftCorner).toBe('top-left');
+})
+
+test('Top side should be rendered', () => {
+    const topSide = calcTileType(5, 64);
+    expect(topSide).toBe('top');
+})
+
+test('Upper right corner should be rendered', () => {
+    const upperRightCorner = calcTileType(7, 64);
+    expect(upperRightCorner).toBe('top-right');
+})
+
+test('Left side should be rendered', () => {
+    const leftSide = calcTileType(8, 64);
+    expect(leftSide).toBe('left');
+})
+
+test('Center should be rendered', () => {
+    const center = calcTileType(35, 64);
+    expect(center).toBe('center');
+})
+
+test('Right side should be rendered', () => {
+    const rightSide = calcTileType(15, 64);
+    expect(rightSide).toBe('right');
+})
+
+test('Bottom left corner should be rendered', () => {
+    const bottomLeftCorner = calcTileType(56, 64);
+    expect(bottomLeftCorner).toBe('bottom-left');
+})
+
+test('Bottom side should be rendered', () => {
+    const bottomSide = calcTileType(60, 64);
+    expect(bottomSide).toBe('bottom');
+})
+
+test('Bottom right corner should be rendered', () => {
+    const bottomrightCorner = calcTileType(63, 64);
+    expect(bottomrightCorner).toBe('bottom-right');
+})
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,6 +1,45 @@
 export function calcTileType(index, boardSize) {
   // TODO: write logic here
-  return 'center';
+  if (index === 0) {
+    return 'top-left';
+  }
+
+  if (index > 0 && index < 7) {
+    return 'top';
+  }
+
+  if (index === 7) {
+    return 'top-right';
+  }
+
+  if (index === 15 || index === 23 || index === 31 || index === 39 || index === 47 || index === 55) {
+    return 'right';
+  }
+
+  if (index === 8 || index === 16 || index === 24 || index === 32 || index === 40 || index === 48) {
+    return 'left';
+  }
+
+  if ((index > 8 && index < 15) || 
+    (index > 16 && index < 23) || 
+    (index > 24 && index < 31) || 
+    (index > 32 && index < 39) ||
+    (index > 40 && index < 47) ||
+    (index > 48 && index < 55)) {
+    return 'center';
+  }
+
+  if (index === 56) {
+    return 'bottom-left';
+  }
+
+  if (index > 56 && index < 63) {
+    return 'bottom';
+  }
+
+  if (index === 63) {
+    return 'bottom-right';
+  }
 }
 
 export function calcHealthLevel(health) {
