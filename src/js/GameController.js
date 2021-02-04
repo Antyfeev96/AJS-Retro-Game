5356e3e2f26c8a73702de23656c4ffddc61e08b4
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-02-04 14:47:22 +0300

    travel radius and not-allowed cursor added

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -12,6 +12,14 @@ export default class GameController {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
     this.allowedArr = [new Bowman(), new Swordsman(), new Undead(), new Vampire()];
+    this.playerCells = [0, 1, 8, 9, 16, 17, 24, 25, 32, 33, 40, 41, 48, 49, 56, 57];
+    this.enemyCells = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63];
+    this.playerFirstCell = this.playerCells[Math.floor(Math.random() * this.playerCells.length)];
+    this.playerCells.splice(this.playerCells.indexOf(this.playerFirstCell, 0), 1);
+    this.playerSecondCell = this.playerCells[Math.floor(Math.random() * this.playerCells.length)];
+    this.enemyFirstCell = this.enemyCells[Math.floor(Math.random() * this.enemyCells.length)];
+    this.enemyCells.splice(this.enemyCells.indexOf(this.enemyFirstCell, 0), 1);
+    this.enemySecondCell = this.enemyCells[Math.floor(Math.random() * this.enemyCells.length)];
   }
 
   init() {
@@ -19,33 +27,14 @@ export default class GameController {
     // TODO: load saved stated from stateService
     this.gamePlay.drawUi(themes.prairie);
 
-    const playerCells = [0, 1, 8, 9, 16, 17, 24, 25, 32, 33, 40, 41, 48, 49, 56, 57];
-    const enemyCells = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63];
-
     const teams = generateTeam(this.allowedArr, 4, 10);
-
-    // player cells
-
-    const playerFirstCell = playerCells[Math.floor(Math.random() * playerCells.length)];
-
-    playerCells.splice(playerCells.indexOf(playerFirstCell, 0), 1);
-
-    const playerSecondCell = playerCells[Math.floor(Math.random() * playerCells.length)];
-
-    // enemy cells
-
-    const enemyFirstCell = enemyCells[Math.floor(Math.random() * enemyCells.length)];
-
-    enemyCells.splice(enemyCells.indexOf(enemyFirstCell, 0), 1);
-
-    const enemySecondCell = enemyCells[Math.floor(Math.random() * enemyCells.length)];
 
     // drawing
     this.gamePlay.redrawPositions([
-      new PositionedCharacter(teams[0][0], playerFirstCell),
-      new PositionedCharacter(teams[0][1], playerSecondCell),
-      new PositionedCharacter(teams[1][0], enemyFirstCell),
-      new PositionedCharacter(teams[1][1], enemySecondCell),
+      new PositionedCharacter(teams[0][0], this.playerFirstCell),
+      new PositionedCharacter(teams[0][1], this.playerSecondCell),
+      new PositionedCharacter(teams[1][0], this.enemyFirstCell),
+      new PositionedCharacter(teams[1][1], this.enemySecondCell),
     ]);
 
     // adding eventListeners
@@ -56,6 +45,11 @@ export default class GameController {
 
   onCellClick(index) {
     // TODO: react to click
+
+    if (this.gamePlay.boardEl.style.cursor === 'not-allowed') {
+      alert('Impossible action');
+    }
+
     if (!Array.from(this.gamePlay.boardEl.children)[index].querySelector('.character')) {
       return;
     }
@@ -79,26 +73,101 @@ export default class GameController {
   onCellEnter(index) {
     // TODO: react to mouse enter
     const cells = Array.from(this.gamePlay.boardEl.children);
+    const charCell = cells[index].querySelector('.character');
     if (this.gamePlay.boardEl.querySelector('.selected-yellow') !== null) {
-      if (!cells[index].querySelector('.character')) {
+      if (!charCell) {
         const selectedCell = cells.indexOf(this.gamePlay.boardEl.querySelector('.selected-yellow'));
-        const arr = [
-          selectedCell - 1, selectedCell - 2, selectedCell - 3, selectedCell - 4,
-          selectedCell + 1, selectedCell + 2, selectedCell + 3, selectedCell + 4,
-          selectedCell - 7, selectedCell - 14, selectedCell - 21, selectedCell - 28,
-          selectedCell + 7, selectedCell + 14, selectedCell + 21, selectedCell + 28,
-          selectedCell - 8, selectedCell - 16, selectedCell - 24, selectedCell - 32,
-          selectedCell + 8, selectedCell + 16, selectedCell + 24, selectedCell + 32,
-          selectedCell - 9, selectedCell - 18, selectedCell - 27, selectedCell - 36,
-          selectedCell + 9, selectedCell + 18, selectedCell + 27, selectedCell + 36,
-        ];
-        arr.forEach((item) => {
-          if (index === item) {
-            this.gamePlay.setCursor('pointer');
-            this.gamePlay.selectCell(index, 'green');
-          }
-        });
-      } else {
+        const x = Math.floor(selectedCell / 8);
+        const y = selectedCell % 8;
+
+        // travel and attack radius for swordsman
+        if (this.gamePlay.boardEl.querySelector('.selected-yellow').querySelector('.swordsman') !== null) {
+          const travelBinaryArr = [
+            [x + 1, y], [x + 2, y], [x + 3, y], [x + 4, y],
+            [x - 1, y], [x - 2, y], [x - 3, y], [x - 4, y],
+            [x, y + 1], [x, y + 2], [x, y + 3], [x, y + 4],
+            [x, y - 1], [x, y - 2], [x, y - 3], [x, y - 4],
+            [x + 1, y - 1], [x + 2, y - 2], [x + 3, y - 3], [x + 4, y - 4],
+            [x - 1, y + 1], [x - 2, y + 2], [x - 3, y + 3], [x - 4, y + 4],
+            [x + 1, y + 1], [x + 2, y + 2], [x + 3, y + 3], [x + 4, y + 4],
+            [x - 1, y - 1], [x - 2, y - 2], [x - 3, y - 3], [x - 4, y - 4],
+          ];
+
+          this.gamePlay.setCursor('not-allowed');
+
+          travelBinaryArr.forEach((item) => {
+            if (Math.floor(index / 8) === item[0] && index % 8 === item[1]) {
+              this.gamePlay.setCursor('pointer');
+              this.gamePlay.selectCell(index, 'green');
+            }
+          });
+
+          const attackBinaryArr = [
+            [x + 1, y],
+            [x - 1, y],
+            [x, y + 1],
+            [x, y - 1],
+            [x + 1, y - 1],
+            [x - 1, y + 1],
+            [x + 1, y + 1],
+            [x - 1, y - 1],
+          ];
+
+          attackBinaryArr.forEach((item) => {
+            if (Math.floor(index / 8) === item[0] && index % 8 === item[1]) {
+              this.gamePlay.setCursor('pointer');
+              this.gamePlay.selectCell(index, 'green');
+            }
+          });
+        }
+
+        // travel radius for bowman
+        if (this.gamePlay.boardEl.querySelector('.selected-yellow').querySelector('.bowman') !== null) {
+          const binaryArr = [
+            [x + 1, y], [x + 2, y],
+            [x - 1, y], [x - 2, y],
+            [x, y + 1], [x, y + 2],
+            [x, y - 1], [x, y - 2],
+            [x + 1, y - 1], [x + 2, y - 2],
+            [x - 1, y + 1], [x - 2, y + 2],
+            [x + 1, y + 1], [x + 2, y + 2],
+            [x - 1, y - 1], [x - 2, y - 2],
+          ];
+
+          this.gamePlay.setCursor('not-allowed');
+
+          binaryArr.forEach((item) => {
+            if (Math.floor(index / 8) === item[0] && index % 8 === item[1]) {
+              this.gamePlay.setCursor('pointer');
+              this.gamePlay.selectCell(index, 'green');
+            }
+          });
+        }
+
+        // travel radius for magician
+        if (this.gamePlay.boardEl.querySelector('.selected-yellow').querySelector('.magician') !== null) {
+          const binaryArr = [
+            [x + 1, y],
+            [x - 1, y],
+            [x, y + 1],
+            [x, y - 1],
+            [x + 1, y - 1],
+            [x - 1, y + 1],
+            [x + 1, y + 1],
+            [x - 1, y - 1],
+          ];
+
+          this.gamePlay.setCursor('not-allowed');
+
+          binaryArr.forEach((item) => {
+            if (Math.floor(index / 8) === item[0] && index % 8 === item[1]) {
+              this.gamePlay.setCursor('pointer');
+              this.gamePlay.selectCell(index, 'green');
+            }
+          });
+        }
+      } else if (charCell) {
+        console.log(charCell.classList[1]);
         const char = cells[index].firstChild.classList[1];
 
         this.allowedArr.forEach((item) => {
